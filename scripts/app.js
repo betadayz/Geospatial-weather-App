e93fce59bdc099a6b83f83f3513b7fc0e63b3883
betadayz <olayinka.joe.sunday@gmail.com>
2023-12-14 08:34:16 +0100

    Creating the event listener

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+const cityForm = document.querySelector('form');
+
+const updateCity = async (city) => {
+
+    const cityDets = await getCity(city)
+};
+
+cityForm.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update the ui with new city
+    updateCity(city);
+
+})
