9ff413b1f060daf2fb4aaef47293e59186949e3e
betadayz <olayinka.joe.sunday@gmail.com>
2023-12-16 10:22:30 +0100

    Setting up the day time and night time with the image

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,28 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
-const ddetails = document.querySelector('.details');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img')
 
 const updateUI = (data) => {
- 
-    const cityDets = data.cityDets
+    
+    const { cityDets, weather } = data;
 
     // update details tempalte
     details.innerHTML = `
     <h5 class="my-3">${cityDets.EnglishName}</h5>
-                <div class="my-3">Weather condition</div>
+                <div class="my-3">${weather.WeatherText}</div>
                 <div class="display-4 my-4">
-                    <span>temp</span>
+                    <span>${weather.Temperature.Metric.Value}</span>
                     <span>&deg;C</span>
                 </div>`;
+    
+    // update the night/day & icon images
+
+    // remove the d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }            
 };
 
 const updateCity = async (city) => {
